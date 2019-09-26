ed1dab2e06e385a1c5b426cc6286284a1224494a
Max-F <max.fullmer@outlook.com>
2019-09-25 20:39:59 -0400

    styling development underway, basic site structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './Components/CheckoutForm';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-        <div className="example">
-          <h1>React Stripe Elements Example</h1>
-          <Elements>
-            <CheckoutForm />
-          </Elements>
+      <main>
+        <header>
+          <h1>Stay Awake</h1>
+        </header>
+
+        <div id="search-n-buy">
+          <section id="category-shop">
+            <h2>Shop Max</h2>
+            <div id="category-wrapper">
+              <article className="categories">Category 1</article>
+              <article className="categories">Category 2</article>
+              <article className="categories">Category 3</article>
+              <article className="categories">Category 4</article>
+            </div>
+          </section>
+
+          <aside id="quick-buy">
+            <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+              <div className="example">
+                <h2>QuickBuy</h2>
+                <Elements>
+                  <CheckoutForm />
+                </Elements>
+              </div>
+            </StripeProvider>
+          </aside>
         </div>
-      </StripeProvider>
+      </main>
     );
   }
 }
