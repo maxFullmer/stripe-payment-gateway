c2899866abff4f5f6673cfb0739cb9c863a341ed
Max-F <max.fullmer@outlook.com>
2019-10-11 17:58:03 -0400

    updated views

--- src/Components/Catalog/Catalog.js
+++ src/Components/Catalog/Catalog.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 class Catalog extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { REDUX STATE ONCE CONNECTED };
-    // }
+    constructor(props) {
+        super(props);
+        this.state = { 
+            loading: true,
+            items: {
+
+            }
+        };
+    }
+    // go to checkout with item that is stored in local cache/storage. "items" = {image, price}
+    checkoutThisItem = () => {
+        // update redux state with selected item (itemReducer.selectedItem: "" => "productID")
+            this.props.history.push('/checkout')
+    }
+
     render() {
         return (
             <section id="item-shop">
-                <header>
+                <header id="catalog-header">
                     <h1>Seed Commerce</h1>
                 </header>
 
                 <h2>Click, Buy, Plant!</h2>
                 <div id="item-wrapper">
-                    <article className="items">item 1</article>
-                    <article className="items">item 2</article>
-                    <article className="items">item 3</article>
-                    <article className="items">item 4</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 1</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 2</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 3</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 4</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 5</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 6</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 7</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 8</article>
+                    <article onClick={this.checkoutThisItem} className="items">item 9</article>
                 </div>
             </section>
         );
     }
 }
 
-export default Catalog;
+export default withRouter(Catalog);
--- src/Components/Checkout/SelectedProduct.js
+++ src/Components/Checkout/SelectedProduct.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
 class SelectedProduct extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { REDUXSTATE ONCE CONNECTED };
+    constructor(props) {
+        super(props);
+        this.state = { quantity: 1 };
+    }
+
+    closeCheckoutView = () => {
+        this.props.history.push('/');
+    }
+
+    // incrementQuantity = () => {
+    //     this.setState({
+    //         quantity: this.state.quantity + 1
+    //     })
     // }
+    // decrementQuantity = () => {
+    //     if (this.state.quantity > 0 )
+    //     this.setState({
+    //         quantity: this.state.quantity - 1
+    //     })
+    // }
+
     render() {
         return (
             <article>
                 <h2 className="header-align">Selected Seed Product</h2>
+                <input onClick={this.closeCheckoutView} type="button" value="X" />
+                <input type="button" value="Add To Cart" />
+                <label htmlFor="quantity">Quantity</label>
+                <input autoFocus type="number" id="quantity" min="1"/>
             </article>
         );
     }
 }
+//connect redux state to Props
 
-export default SelectedProduct;
+export default withRouter(SelectedProduct);
